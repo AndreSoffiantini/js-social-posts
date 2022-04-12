13001ab69241000abf268dd609abd93f5c9f9da6
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-04-12 19:18:03 +0200

    Add more posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,11 @@ numero di likes. */
 const posts = [{
         id: 1,
         author_name: "Andrea Soffiantini",
-        author_photo: "https://picsum.photos/536/354",
+        author_photo: "",
         date: "04-12-2022",
         post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
         post_image: "https://picsum.photos/536/355",
-        likes: 60
+        likes: 0
     },
     {
         id: 2,
@@ -25,7 +25,79 @@ const posts = [{
         date: "04-12-2022",
         post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
         post_image: "https://picsum.photos/536/357",
-        likes: 50
+        likes: 0
+    },
+    {
+        id: 3,
+        author_name: "Luca Verdi",
+        author_photo: "https://picsum.photos/536/358",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/359",
+        likes: 0
+    },
+    {
+        id: 4,
+        author_name: "Matteo Bianchi",
+        author_photo: "https://picsum.photos/536/360",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/361",
+        likes: 0
+    },
+    {
+        id: 5,
+        author_name: "Faolo Pazio",
+        author_photo: "https://picsum.photos/536/362",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: " ",
+        likes: 0
+    },
+    {
+        id: 6,
+        author_name: "Paolo Mieli",
+        author_photo: "https://picsum.photos/536/364",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/365",
+        likes: 0
+    },
+    {
+        id: 7,
+        author_name: "Amerigo Vespucci",
+        author_photo: "https://picsum.photos/536/366",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/367",
+        likes: 0
+    },
+    {
+        id: 8,
+        author_name: "Tonio Cartonio",
+        author_photo: "https://picsum.photos/536/368",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/369",
+        likes: 0
+    },
+    {
+        id: 9,
+        author_name: "Walter Veltroni",
+        author_photo: "https://picsum.photos/536/370",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/371",
+        likes: 0
+    },
+    {
+        id: 10,
+        author_name: "Amedeo Della Valle",
+        author_photo: "https://picsum.photos/536/372",
+        date: "04-12-2022",
+        post_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum odio vitae felis ultrices, tempus sagittis mi lobortis. In ac elit non enim semper auctor. Donec vel dictum metus. Duis sem erat, rutrum non diam vitae, luctus dictum mauris. Cras eget rhoncus quam. Integer sagittis ipsum sit amet faucibus ornare.",
+        post_image: "https://picsum.photos/536/373",
+        likes: 0
     }
 ]
 
@@ -52,7 +124,11 @@ function printPost(post) {
     postSection.id = `${post.id}`;
     mainContainer.appendChild(postSection);
 
-    // Generare testo random da inserire nel post
+    // Gestire l'assenza dell'immagine del post
+
+    /* BONUS 2:
+    Gestire l'assenza dell'immagine profilo con un elemento di fallback 
+    che contiene le iniziali dell'utente (es. Luca Formicola > LF). */
 
     // Inserire il contenuto del post all'interno della sezione, estraendo i dati dall'array
     postSection.innerHTML = `
@@ -135,7 +211,8 @@ function handleLike(event) {
     // Salvare nell'apposito array l'id del post piaciuto
     liked_posts.push(post_id);
     liked_posts.sort();
-    console.log(liked_posts);
+    console.clear();
+    console.log(`Liked posts IDs: ${liked_posts}`);
 
     /* BONUS 3
     Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato 
@@ -173,7 +250,8 @@ function handleDislike(event) {
     // Rimuovere l'id del post dall'array dei post piaciuti 
     liked_posts = liked_posts.filter(id => id !== post_id);
     liked_posts.sort();
-    console.log(liked_posts);
+    console.clear();
+    console.log(`Liked posts IDs: ${liked_posts}`);
 
     // Rimuovere l'eventListener per gestire il dislike
     event.path[0].removeEventListener("click", handleDislike);
